3440725f03f3fdf419fbe52b78a56ba7049eefb1
Clt689 <kx1302@naver.com>
2024-01-08 18:43:54 +0900

    5. props (속성)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 // 리액트에서 사용자 정의 태그를 만들 때는 반드시 대문자로 시작
-function Header() {
+function Header(props) {
+  console.log('props', props, props.title);
   return <header>
-    <h1><a href="/">WEB</a></h1>
+    <h1><a href="/">{props.title}</a></h1>
   </header>
 }
 
-function Nav() {
+function Nav(props) {
   return <nav>
     <ol>
       <li><a href="/read/1">html</a></li>
@@ -18,21 +19,22 @@ function Nav() {
   </nav>
 }
 
-function Article() {
+function Article(props) {
   return <article>
-    <h2>Welcome</h2>
-    Hello, WEB
+    <h2>{props.title}</h2>
+    {props.body}
   </article>
 }
 
 function App() {
   return (
     <div>
-      <Header></Header>
+      <Header title="REACT"></Header>
       <Nav></Nav>
-      <Article></Article>
+      <Article title="Welcome" body="Hello, WEB"></Article>
     </div>
   );
 }
 
+// 리액트는 속성을 PROP이라고 함.
 export default App;
