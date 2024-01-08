d566a7dae87fdc4ee3d22b2f4724bedf963b6424
Clt689 <kx1302@naver.com>
2024-01-08 16:11:39 +0900

    4. 컴포넌트 만들기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
+// 리액트에서 사용자 정의 태그를 만들 때는 반드시 대문자로 시작
+function Header() {
+  return <header>
+    <h1><a href="/">WEB</a></h1>
+  </header>
+}
+
+function Nav() {
+  return <nav>
+    <ol>
+      <li><a href="/read/1">html</a></li>
+      <li><a href="/read/2">css</a></li>
+      <li><a href="/read/3">js</a></li>
+    </ol>
+  </nav>
+}
+
+function Article() {
+  return <article>
+    <h2>Welcome</h2>
+    Hello, WEB
+  </article>
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
