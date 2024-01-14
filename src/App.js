e206fc77f20a67b94c3c0c32d87effa10fc7615b
Clt689 <kx1302@naver.com>
2024-01-14 20:48:54 +0900

    5. props

--- src/App.js
+++ src/App.js
@@ -1,52 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
- 
- 
+
+// Header, Nav, Article = 함수형 컴포넌트
+function Header(props){
+  console.log('props', props, props.title);    // props객체를 콘솔에 출력함(디버깅 목적)
+  return <header>                    
+    <h1><a href="/">{props.title}</a></h1>   
+  </header>
+}   // JSX(JavaScript Xml) - 리액트에서 HTML 작성하는 방식 의미
+
+function Nav(){
+  return <nav>
+    <ol>
+      <li><a href="/read/1">html</a></li>
+      <li><a href="/read/2">css</a></li>
+      <li><a href="/read/3">javascript</a></li>
+    </ol>
+  </nav>
+}
+
 function Article(props){
   return <article>
     <h2>{props.title}</h2>
     {props.body}
   </article>
 }
-function Header(props){
-  return <header>
-    <h1><a href="/" onClick={(event)=>{
-      event.preventDefault();
-      props.onChangeMode();
-    }}>{props.title}</a></h1>
-  </header>
-}
-function Nav(props){
-  const lis = []
-  for(let i=0; i<props.topics.length; i++){
-    let t = props.topics[i];
-    lis.push(<li key={t.id}>
-      <a id={t.id} href={'/read/'+t.id} onClick={event=>{
-        event.preventDefault();
-        props.onChangeMode(event.target.id);
-      }}>{t.title}</a>
-    </li>)
-  }
-  return <nav>
-    <ol>
-      {lis}
-    </ol>
-  </nav>
-}
+
 function App() {
-  const topics = [
-    {id:1, title:'html', body:'html is ...'},
-    {id:2, title:'css', body:'css is ...'},
-    {id:3, title:'javascript', body:'javascript is ...'}
-  ] 
   return (
     <div>
-      <Header title="WEB" onChangeMode={()=>{
-        alert('Header');
-      }}></Header>
-      <Nav topics={topics} onChangeMode={(id)=>{
-        alert(id);
-      }}></Nav>
+      <Header title="WEB"></Header>
+      <Nav></Nav>
       <Article title="Welcome" body="Hello, WEB"></Article>
     </div>
   );
