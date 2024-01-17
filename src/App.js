419f989bef1ec1bf6adf54cfbe40069acbb4bc84
Clt689 <kx1302@naver.com>
2024-01-17 18:40:00 +0900

    8. Create

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function Article(props) {
 function Header(props) {
   console.log('props', props, props.title);    // props객체를 콘솔에 출력함(디버깅 목적)
   return <header>
-    <h1><a href="/" onClick={(event)=>{
+    <h1><a href="/" onClick={(event) => {
       event.preventDefault();
       props.onChangeMode();
     }}>{props.title}</a></h1>
@@ -25,11 +25,11 @@ function Nav(props) {
   for (let i = 0; i < props.topics.length; i++) {
     let t = props.topics[i];
     lis.push(<li key={t.id}>
-      <a id={t.id} href={'read/' + t.id} onClick={event=>{
+      <a id={t.id} href={'read/' + t.id} onClick={event => {
         event.preventDefault();
         props.onChangeMode(Number(event.target.id));
       }}>{t.title}</a>
-      </li>)   // 동적으로 만들어주는 태그 
+    </li>)   // 동적으로 만들어주는 태그 
   }
   return <nav>
     <ol>
@@ -38,44 +38,65 @@ function Nav(props) {
   </nav>
 }
 
+function Create(props) {
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={event=>{
+      event.preventDefault();         // create를 눌러 제출할 때, reload가 일어나지 않게 하기 위함.
+      const title = event.target.title.value;    // 이벤트가 발생한 타겟 => <form>태그를 가리킴    
+      const body = event.target.body.value;
+      props.onCreate(title, body);
+    }}>
+      <p><input type="text" name="title" placeholder='title' /></p>   {/* 제목 */}
+      <p><textarea name="body" placeholder='body'></textarea></p>    {/* 본문 */}
+      <p><input type="submit" value="Create"></input></p>
+    </form>
+  </article>
+}
+
 function App() {                         // useState의 인자(='WELCOME')는 state의 초기값 = 0번쨰 인덱스의 값 = mode
   // const _mode = useState('WELCOME');     // 일반 지역변수지만, state'상태'로 업그레이드
   // const mode = _mode[0];       // 배열의 0번째 => 상태의 값을 읽을 때 사용 / 1번째 => 상태의 값을 변경할 때 사용
   // console.log('_mode', _mode);
   const [mode, setMode] = useState('WELCOME');   // 위 3줄과 같은 코드
   const [id, setId] = useState(null);
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     { id: 1, title: 'html', body: 'html is ...' },
     { id: 2, title: 'css', body: 'css is ...' },
     { id: 3, title: 'javascript', body: 'javascript is ...' },
-  ]
+  ]);
   let content = null;
-  if ( mode === 'WELCOME'){
+  if (mode === 'WELCOME') {
     content = <Article title="Welcome" body="Hello, WEB"></Article>
-  } else if ( mode === 'READ'){
+  } else if (mode === 'READ') {
     let title, body = null;
-    for(let i=0; i<topics.length; i++){
+    for (let i = 0; i < topics.length; i++) {
       console.log(topics[i].id, id);
-      if(topics[i].id === id){
+      if (topics[i].id === id) {
         title = topics[i].title;
         body = topics[i].body;
       }
     }
     content = <Article title={title} body={body}></Article>
-  } else if(mode === 'CREATE'){
-    content = 
+  } else if (mode === 'CREATE') {
+    content = <Create onCreate={(_title, _body) => {
+      const newTopic = {id:nextId, title:_title, body:_body};
+      topics.push(newTopic);
+      setTopics(topics);
+    }}></Create>  // Create는 꽤 복잡한 UI를 갖고있기 때문에, 별도의 컴포넌트<Create>로 만듦
   }
   return (
     <div>
-      <Header title="WEB" onChangeMode={()=>{
+      <Header title="WEB" onChangeMode={() => {
         setMode('WELCOME');
       }}></Header>
-      <Nav topics={topics} onChangeMode={(_id)=>{
+      <Nav topics={topics} onChangeMode={(_id) => {
         setMode('READ');
         setId(_id);
       }}></Nav>    {/* "topics"가 아니라 {}로 하면 문자열이 아니라 있는 그대로 전달됨. */}
       {content}
-      <a href="/create" onClick={event=>{
+      <a href="/create" onClick={event => {
         event.preventDefault();
         setMode('CREATE');
       }}>Create</a>
