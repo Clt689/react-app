349f460df060c213e83dcb3a90472d7f2f4ab194
Clt689 <kx1302@naver.com>
2024-01-21 16:42:33 +0900

    8. Create 복습1

--- src/App.js
+++ src/App.js
@@ -41,6 +41,11 @@ function Nav(props) {
 function Create(){
   return <article>
     <h2>Create</h2>
+    <form>
+      <p><input type="text" name="title" placeholder='title'/></p>
+      <p><textarea name="body" placeholder='body'></textarea></p>
+      <p><input type="submit" value="Create"></input></p>
+    </form>
   </article>
 }
 
