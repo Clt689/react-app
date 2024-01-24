71c23c7da74dd78b5523e6d1d7b476f6d4d0bb97
Clt689 <kx1302@naver.com>
2024-01-24 18:58:24 +0900

    10. Delete

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function Create(props) {
   </article>
 }
 
-function Update(props){
+function Update(props) {
   const [title, setTitle] = useState(props.title);
   const [body, setBody] = useState(props.body);
   return <article>
@@ -65,11 +65,11 @@ function Update(props){
       const body = event.target.body.value;
       props.onUpdate(title, body);
     }}>
-      <p><input type="text" name="title" placeholder='title' value={title} onChange={event=>{
+      <p><input type="text" name="title" placeholder='title' value={title} onChange={event => {
         console.log(event.target.value);
         setTitle(event.target.value);
-      }}/></p>
-      <p><textarea name="body" placeholder='body' value={body} onChange={event=>{
+      }} /></p>
+      <p><textarea name="body" placeholder='body' value={body} onChange={event => {
         setBody(event.target.value);
       }}></textarea></p>
       <p><input type="submit" value="Update"></input></p>
@@ -102,61 +102,73 @@ function App() {                         // useState의 인자(='WELCOME')는 st
       }
     }
     content = <Article title={title} body={body}></Article>
-    contextControl = <li><a href={'/update/'+id} onClick={event=>{
-      event.preventDefault();
-      setMode('UPDATE');
-    }}>Update</a></li>
+    contextControl = <>
+      <li><a href={'/update/' + id} onClick={event => {
+        event.preventDefault();
+        setMode('UPDATE');
+      }}>Update</a></li>
+      <li><input type="button" value="delete" onClick={()=>{
+        const newTopics = []
+        for(let i = 0; i < topics.length; i++){
+          if( topics[i].id !== id ){
+            newTopics.push(topics[i]);
+          }
+        }
+        setTopics(newTopics);
+        setMode('WELCOME');
+      }}/></li>
+    </>
   } else if (mode === "CREATE") {
-    content = <Create onCreate={(_title, _body) => {
-      const newTopic = { id: nextId, title: _title, body: _body }
-      const newTopics = [...topics]             // topics를 복제한 복제본
-      newTopics.push(newTopic);
-      setTopics(newTopics);
-      setMode('READ');
-      setId(nextId);
-      setNextId(nextId + 1);
-    }}></Create>
-  } else if ( mode === "UPDATE"){
-    let title, body = null;
-    for (let i = 0; i < topics.length; i++) {
+        content = <Create onCreate={(_title, _body) => {
+          const newTopic = { id: nextId, title: _title, body: _body }
+          const newTopics = [...topics]             // topics를 복제한 복제본
+          newTopics.push(newTopic);
+          setTopics(newTopics);
+          setMode('READ');
+          setId(nextId);
+          setNextId(nextId + 1);
+        }}></Create>
+      } else if ( mode === "UPDATE"){
+        let title, body = null;
+      for (let i = 0; i < topics.length; i++) {
       if (topics[i].id === id) {
         title = topics[i].title;
-        body = topics[i].body;
+      body = topics[i].body;
       }
     }
-    content = <Update title={title} body={body} onUpdate={(title,body)=>{
-      console.log(title, body);
-      const newTopics = [...topics];
-      const updatedTopic = {id:id, title:title, body:body}
-      for(let i = 0; i < newTopics.length; i++){
-        if( newTopics[i].id === id){
-          newTopics[i] = updatedTopic;
-          break;
+      content = <Update title={title} body={body} onUpdate={(title, body) => {
+        console.log(title, body);
+        const newTopics = [...topics];
+        const updatedTopic = { id: id, title: title, body: body }
+        for (let i = 0; i < newTopics.length; i++) {
+          if (newTopics[i].id === id) {
+            newTopics[i] = updatedTopic;
+            break;
+          }
         }
-      }
-      setTopics(newTopics);
-      setMode('READ');
-    }}></Update>
-  }
-  return (
-    <div>
-      <Header title="WEB" onChangeMode={() => {
-        setMode('WELCOME');
-      }}></Header>
-      <Nav topics={topics} onChangeMode={(_id) => {
+        setTopics(newTopics);
         setMode('READ');
-        setId(_id);
-      }}></Nav>    {/* "topics"가 아니라 {}로 하면 문자열이 아니라 있는 그대로 전달됨. */}
-      {content}
-      <ul>
-        <li><a href="/create" onClick={event => {
-          event.preventDefault();        // Create 링크 클릭했을 때, url이 바뀌지 않도록 처리.
-          setMode('CREATE');
-        }}>Create</a></li>
-        {contextControl}
-      </ul>
-    </div>
-  );
+      }}></Update>
+  }
+      return (
+      <div>
+        <Header title="WEB" onChangeMode={() => {
+          setMode('WELCOME');
+        }}></Header>
+        <Nav topics={topics} onChangeMode={(_id) => {
+          setMode('READ');
+          setId(_id);
+        }}></Nav>    {/* "topics"가 아니라 {}로 하면 문자열이 아니라 있는 그대로 전달됨. */}
+        {content}
+        <ul>
+          <li><a href="/create" onClick={event => {
+            event.preventDefault();        // Create 링크 클릭했을 때, url이 바뀌지 않도록 처리.
+            setMode('CREATE');
+          }}>Create</a></li>
+          {contextControl}
+        </ul>
+      </div>
+      );
 }
 
-export default App;
+      export default App;
