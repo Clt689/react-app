6a6975c88fbed45500765f78042bf1ea7f259430
Nadnerde <115773895+Clt689@users.noreply.github.com>
2024-03-04 16:41:59 +0900

    7장. state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 function Article(props){
   return <article>
     <h2>{props.title}</h2>
@@ -11,7 +12,7 @@ function Header(props){
   return <header>
       <h1><a href="/" onClick={(event)=>{
         event.preventDefault();       // 클릭해도 reload가 일어나지 않음.
-        props.onchangeMode();
+        props.onChangeMode();
       }}>{props.title}</a></h1>
   </header>
 }
@@ -22,7 +23,7 @@ function Nav(props){
    lis.push(<li key={t.id}>
     <a id={t.id} href={'/read/' + t.id} onClick={event=>{
       event.preventDefault();
-      props.onchangeMode(event.target.id);
+      props.onChangeMode(Number(event.target.id));
     }}>{t.title}</a>
     </li>);
   }
@@ -33,20 +34,37 @@ function Nav(props){
   </nav>
 }
 function App() {
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
   const topics = [
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javascript', body:'javascript is ...'},
   ]
+  let content = null;
+  if (mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello, WEB"></Article>
+  } else if (mode === 'READ'){
+    let title, body = null;
+    for(let i=0; i < topics.length; i++){
+      console.log(topics[i].id, id);
+      if(topics[i].id === id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>
+  }
   return(
     <div>
-      <Header title="WEB" onchangeMode={()=>{
-        alert('Header');
+      <Header title="WEB" onChangeMode={()=>{
+        setMode('WELCOME');
       }}></Header>
-      <Nav topics={topics} onchangeMode={(id)=>{
-        alert(id);
+      <Nav topics={topics} onChangeMode={(_id)=>{
+        setMode('READ');
+        setId(_id);
       }}></Nav>
-      <Article title="Welcome" body="Hello, WEB"></Article>
+      {content}
     </div>
   );
 }
